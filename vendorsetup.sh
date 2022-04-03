3c738ea9cfb60e3b23ba4d8d9f5515a8d9b179b8
AdarshGrewal <adarshgrewal@gmail.com>
2022-04-03 19:18:28 +0530

    onyx: Include MiuiCamera if-exists
    Change-Id: I03fe0f13775e74301ca0d3aaca81f0509a5a8cbb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,10 @@ echo 'Cloning Bcr Tree'
 echo 'Cloning Hardware Tree'
         git clone https://github.com/xiaomi-sm8750-onyx/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 
+echo 'Cloning MiuiCamera Tree'
+	git clone https://github.com/xiaomi-sm8750-onyx/android_device_xiaomi_onyx-miuicamera.git -b lineage-23.2 device/xiaomi/onyx-miuicamera
+	git clone https://github.com/xiaomi-sm8750-onyx/android_vendor_xiaomi_onyx-miuicamera.git -b lineage-23.2 vendor/xiaomi/onyx-miuicamera
+
 echo 'Cloning Prebuilt Kernel Tree'
 	git clone https://github.com/xiaomi-sm8750-onyx/android_device_xiaomi_onyx-kernel.git -b lineage-23.2 device/xiaomi/onyx-kernel
 
