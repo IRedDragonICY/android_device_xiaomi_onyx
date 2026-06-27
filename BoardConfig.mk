80be674288b76872ad345108c4d229e3d59ab3cc
SharmagRit <sharmagritstuff@gmail.com>
2026-06-27 01:24:12 +0000

    onyx: Welcome @Lunaris-AOSP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,7 +156,7 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
--include vendor/infinity/config/BoardConfigReservedSize.mk
+-include vendor/lineage/config/BoardConfigReservedSize.mk
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,7 +14,7 @@ echo 'Cloning Prebuilt Kernel Tree'
 	git clone https://github.com/xiaomi-sm8750-onyx/android_device_xiaomi_onyx-kernel.git -b lineage-23.2 device/xiaomi/onyx-kernel
 
 echo 'Cloning Priv-keys'
-	git clone https://github.com/xiaomi-sm8750-onyx/android_vendor_infinity-priv_keys.git -b lineage-23.2 vendor/infinity-priv/keys
+	git clone https://github.com/xiaomi-sm8750-onyx/android_vendor_sharmagrit-priv_keys.git -b lineage-23.2 vendor/sharmagrit-priv/keys
 
 echo 'Cloning Vendor Tree'
 	git clone https://github.com/xiaomi-sm8750-onyx/android_vendor_xiaomi_onyx.git -b lineage-23.2 vendor/xiaomi/onyx
